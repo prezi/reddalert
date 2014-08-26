1801868a61a51e390f884f0b5568a3c648409923
Attila Zseller <attila.zseller@prezi.com>
2014-08-26 14:12:41 +0200

    enrich instances with elb and securitygroup related data

---
+++ api/instanceenricher.py
@@ -0,0 +1,49 @@
+import operator
+
+class InstanceEnricher:
+
+    def __init__(self, edda_client):
+        self.edda_client = edda_client.clean()
+
+    def initialize_caches(self):
+        self.elbs = self._query_loadbalancers()
+        self.sec_groups = self._query_security_groups()
+
+    def _query_security_groups(self):
+        groups = self.edda_client.query("/api/v2/aws/securityGroups;_expand")
+        return {g["groupId"]: reduce(operator.add, self._clean_ip_permissions(g["ipPermissions"])) for g in groups}
+
+    def _clean_ip_permissions(self, perms):
+        return [self._clean_ip_permission(p) for p in perms]
+
+    def _clean_ip_permission(self, permission):
+        return [{"port": permission["toPort"], "range": r} for r in permission["ipRanges"]]
+
+    def _query_loadbalancers(self):
+        elbs = self.edda_client.query("/api/v2/aws/loadBalancers;_expand")
+        return [self._clean_elb(e) for e in elbs if len(e.get("instances", [])) > 0]
+
+    def _clean_elb(self, elb):
+        return {
+            "DNSName": elb.get("DNSName"),
+            "instances": [i.get("instanceId") for i in elb.get("instances")],
+            "ports": [l.get("loadBalancerPort") for l in elb.get("listenerDescriptions")]
+        }
+
+    def enrich(self, instance_data):
+        instance_id = instance_data.get("instanceId")
+        instance_data["service_type"] = self._get_type_from_tags(instance_data.get("tags", [])) or instance_id
+        instance_data["elbs"] = [elb for elb in self.elbs if instance_id in elb["instances"]]
+        self._enrich_security_groups(instance_data["securityGroups"])
+
+    def _enrich_security_groups(self, security_groups):
+        for sg in security_groups:
+            sg["rules"] = self.sec_groups.get(sg["groupId"], [])
+
+    def _get_type_from_tags(self, tags):
+        LOOKUP_ORDER = ["service_name", "Name", "aws:autoscaling:groupName"]
+        for tag_name in LOOKUP_ORDER:
+            for tag in tags:
+                if tag.get("key") == tag_name:
+                    return tag.get("value")
+        return None
---
+++ tests/api/test_instanceenricher.py
@@ -0,0 +1,120 @@
+import unittest
+from mock import Mock
+
+from api.instanceenricher import InstanceEnricher
+
+class InstanceEnricherTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.edda_client = Mock()
+        edda_outer = Mock()
+        edda_outer.clean = Mock(return_value=self.edda_client)
+        self.instance_enricher = InstanceEnricher(edda_outer)
+
+    def test_query_securitygroups(self):
+        SECURITY_GROUPS = [
+            {
+                "vpcId": None,
+                "class": "com.amazonaws.services.ec2.model.SecurityGroup",
+                "description": "security-log-source",
+                "groupId": "sg-XXXXX1",
+                "groupName": "security-log-source",
+                "ipPermissions": [
+                    {
+                        "userIdGroupPairs": [],
+                        "toPort": 22,
+                        "ipRanges": [
+                            "0.0.0.0/0"
+                        ],
+                        "ipProtocol": "tcp",
+                        "fromPort": 22,
+                        "class": "com.amazonaws.services.ec2.model.IpPermission"
+                    }
+                ],
+                "ipPermissionsEgress": [],
+                "ownerId": "123",
+                "tags": []
+            },
+            {
+                "vpcId": None,
+                "class": "com.amazonaws.services.ec2.model.SecurityGroup",
+                "description": "security-log-drain",
+                "groupId": "sg-XXXXX2",
+                "groupName": "security-log-drain",
+                "ipPermissions": [
+                    {
+                        "userIdGroupPairs": [],
+                        "toPort": 22,
+                        "ipRanges": [
+                            "0.0.0.0/0"
+                        ],
+                        "ipProtocol": "tcp",
+                        "fromPort": 22,
+                        "class": "com.amazonaws.services.ec2.model.IpPermission"
+                    },
+                    {
+                        "userIdGroupPairs": [],
+                        "toPort": 22,
+                        "ipRanges": [
+                            "10.1.0.0/16",
+                            "192.168.0.0/16",
+                        ],
+                        "ipProtocol": "tcp",
+                        "fromPort": 22,
+                        "class": "com.amazonaws.services.ec2.model.IpPermission"
+                    }
+                ],
+                "ipPermissionsEgress": [],
+                "ownerId": "123",
+                "tags": []
+            },
+        ]
+        self.edda_client.query = Mock(return_value=SECURITY_GROUPS)
+
+        sgs = self.instance_enricher._query_security_groups()
+
+        self.assertIsInstance(sgs, dict)
+        self.assertIn("sg-XXXXX1", sgs)
+        self.assertIn("sg-XXXXX2", sgs)
+        self.assertEqual(3, len(sgs["sg-XXXXX2"]))
+
+    def test_enrich(self):
+        self.instance_enricher.elbs = [
+            {"DNSName": "foo.prezi.com", "instances": ["A", "B"], "ports": ["80", "81"]},
+            {"DNSName": "bar.prezi.com", "instances": ["A", "C"], "ports": ["80", "81"]}
+        ]
+        self.instance_enricher.sec_groups = {
+            "sg-XXXXX1": [{"port": "22", "range": "0.0.0.0/0"}]
+        }
+        INSTANCE_DATA = {
+            "tags": [
+                {
+                    "value": "lucid",
+                    "maksim_node_type": "lucid",
+                    "key": "maksim_node_type",
+                    "class": "com.amazonaws.services.ec2.model.Tag"
+                },
+                {
+                    "value": "jenkins",
+                    "service_name": "jenkins",
+                    "key": "service_name",
+                    "class": "com.amazonaws.services.ec2.model.Tag"
+                }
+            ],
+            "instanceId": "A",
+            "securityGroups": [
+                {
+                    "groupName": "jenkins",
+                    "groupId": "sg-XXXXX1",
+                    "class": "com.amazonaws.services.ec2.model.GroupIdentifier"
+                }
+            ]
+        }
+
+        self.instance_enricher.enrich(INSTANCE_DATA)
+
+        self.assertIn("elbs", INSTANCE_DATA)
+        self.assertEqual(2, len(INSTANCE_DATA["elbs"]))
+        self.assertIn("rules", INSTANCE_DATA["securityGroups"][0])
+        self.assertEqual(1, len(INSTANCE_DATA["securityGroups"][0]["rules"]))
+        self.assertEqual("jenkins", INSTANCE_DATA["service_type"])
