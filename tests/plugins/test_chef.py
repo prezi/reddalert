117a966d7db9373a48d42c87f3515f5a46e31c33
Attila Zseller <attila.zseller@prezi.com>
2014-12-10 14:18:40 +0100

    conflict resolved

--- tests/plugins/test_chef.py
+++ tests/plugins/test_chef.py
@@ -24,6 +24,44 @@ class PluginNonChefTestCase(unittest.TestCase):
             mock.assert_called_once_with('foo', '<key_file>', 'bar')
 
     @patch('plugins.chef.ChefAPI')
+    def test_handle_invalid_chef_data(self, *mocks):
+        instance_enricher = InstanceEnricher(Mock())
+        eddaclient = Mock()
+
+        def ret_list(args):
+            return [
+                {'keyName': 'keyName1', 'instanceId': 'a', 'privateIpAddress': '10.1.1.1', 'publicIpAddress': '1.1.1.1',
+                 "tags": [{"key": "Name", "value": "tag1"}, {'a': 'b'}], 'launchTime': 1 * 3600000},
+                {'keyName': 'keyName2', 'instanceId': 'b', 'privateIpAddress': '10.1.1.2', 'publicIpAddress': '2.1.1.1',
+                 "tags": [{"key": "service_name", "value": "foo"}], 'launchTime': 1 * 3600000},
+                {'keyName': 'keyName3', 'instanceId': 'c', 'privateIpAddress': '10.1.1.3', 'publicIpAddress': '3.1.1.1',
+                 'launchTime': 1 * 3600000},
+                {'keyName': 'keyName4', 'instanceId': 'd', 'privateIpAddress': '10.1.1.4', 'publicIpAddress': '4.1.1.1',
+                 'launchTime': 1 * 3600000}
+            ]
+
+        m = Mock()
+        m.query = Mock(side_effect=ret_list)
+        eddaclient.soft_clean = Mock(return_value=m)
+        eddaclient._since = 3 * 3600000
+        eddaclient._until = 4 * 3600000 + 1
+
+        def chef_list(*args, **kwargs):
+            return [
+                {'name': 'host3', 'automatic': {'cloud': {'foo': '1.1.1.1'}}},
+                {'name': 'host4', 'automatic': {'foo': {'public_ipv4': '2.1.1.1'}}},
+                {'foo': {'cloud': {'public_ipv4': '3.1.1.1'}}}
+            ]
+
+        with patch('plugins.chef.Search', side_effect=chef_list):
+            self.plugin.init(eddaclient, self.config, {}, instance_enricher)
+
+            alerts = list(self.plugin.do_run())
+
+            # no valid chef data was returned, all the 4 elements are non-chef
+            self.assertEqual(4, len(alerts))
+
+    @patch('plugins.chef.ChefAPI')
     def test_empty_status(self, *mocks):
         instance_enricher = InstanceEnricher(Mock())
         eddaclient = Mock()
@@ -31,41 +69,36 @@ class PluginNonChefTestCase(unittest.TestCase):
         def ret_list(args):
             return [
                 {'keyName': 'keyName1', 'instanceId': 'a', 'privateIpAddress': '10.1.1.1', 'publicIpAddress': '1.1.1.1',
-                 "tags": [{"key": "Name", "value": "tag1"}, {'a': 'b'}]},
+                 "tags": [{"key": "Name", "value": "tag1"}, {'a': 'b'}], 'launchTime': 1 * 3600000},
                 {'keyName': 'keyName2', 'instanceId': 'b', 'privateIpAddress': '10.1.1.2', 'publicIpAddress': '2.1.1.1',
-                 "tags": [{"key": "service_name", "value": "foo"}]},
+                 "tags": [{"key": "service_name", "value": "foo"}], 'launchTime': 1 * 3600000},
                 {'keyName': 'keyName3', 'instanceId': 'c', 'privateIpAddress': '10.1.1.3', 'publicIpAddress': '3.1.1.1',
-                 'securityGroups': ['foobar']},
-                {'keyName': 'keyName4', 'instanceId': 'd', 'privateIpAddress': '10.1.1.4', 'publicIpAddress': '4.1.1.1'}
+                 'launchTime': 1 * 3600000},
+                {'keyName': 'keyName4', 'instanceId': 'd', 'privateIpAddress': '10.1.1.4', 'publicIpAddress': '4.1.1.1',
+                 'launchTime': 1 * 3600000}
             ]
 
         m = Mock()
         m.query = Mock(side_effect=ret_list)
-        eddaclient.clean = Mock(return_value=m)
-        eddaclient._since = 0
+        eddaclient.soft_clean = Mock(return_value=m)
+        eddaclient._since = 3 * 3600000
+        eddaclient._until = 4 * 3600000 + 1
 
         def chef_list(*args, **kwargs):
-            return [{'name': 'host0', 'automatic': {'cloud': {'public_ipv4': '3.1.1.1'}}},
-                    {'name': 'host1', 'automatic': {'cloud': {'public_ipv4': '4.1.1.1'}}},
-                    {'name': 'host2', 'automatic': {'cloud': {'public_ipv4': '10.1.1.1'}}},
-                    {'name': 'host3', 'automatic': {'cloud': {'foo': '2.1.1.1'}}},
-                    {'name': 'host4', 'automatic': {'foo': {'public_ipv4': '2.1.1.1'}}},
-                    {'foo': {'cloud': {'public_ipv4': '2.1.1.1'}}}]
+            return [
+                {'name': 'host0', 'automatic': {'cloud': {'public_ipv4': '1.1.1.1'}}},
+                {'name': 'host1', 'automatic': {'cloud': {'public_ipv4': '2.1.1.1'}}},
+                {'name': 'host2', 'automatic': {'cloud': {'public_ipv4': '5.1.1.1'}}},
+            ]
 
         with patch('plugins.chef.Search', side_effect=chef_list) as MockClass:
             self.plugin.init(eddaclient, self.config, {}, instance_enricher)
 
-            # run the tested method
-            self.assertEqual(list(self.plugin.do_run()), [
-                             {'id': 'keyName1-tag1', 'plugin_name': 'non_chef', 'details': [
-                              {'tags': {'Name': 'tag1'}, 'instanceId': 'a', 'keyName': 'keyName1', 'securityGroups': [],
-                               'publicIpAddress': '1.1.1.1', 'privateIpAddress': '10.1.1.1', 'elbs': [], 'open_ports': [],
-                               'service_type': 'tag1', 'started': 0}]},
-                             {'id': 'keyName2-foo', 'plugin_name': 'non_chef', 'details': [
-                              {'tags': {'service_name': 'foo'}, 'instanceId': 'b', 'keyName': 'keyName2', 'securityGroups': [],
-                               'publicIpAddress': '2.1.1.1', 'privateIpAddress': '10.1.1.2', 'elbs': [], 'open_ports': [],
-                               'service_type': 'foo', 'started': 0}]}
-                             ])
+            alerts = list(self.plugin.do_run())
+            # there are two reportable instances, 3.1.1.1 and 4.1.1.1
+            self.assertEqual(2, len(alerts))
+            self.assertTrue(any(a["details"][0]["publicIpAddress"] == "3.1.1.1" for a in alerts))
+            self.assertTrue(any(a["details"][0]["publicIpAddress"] == "4.1.1.1" for a in alerts))
 
     @patch('plugins.chef.ChefAPI')
     def test_nonempty_status(self, *mocks):
@@ -75,44 +108,37 @@ class PluginNonChefTestCase(unittest.TestCase):
         def ret_list(args):
             return [
                 {'keyName': 'keyName1', 'instanceId': 'a', 'privateIpAddress': '10.1.1.1', 'publicIpAddress': '1.1.1.1',
-                 "tags": [{"key": "Name", "value": "tag1"}, {'a': 'b'}], 'launchTime': 12},
+                 "tags": [{"key": "Name", "value": "tag1"}, {'a': 'b'}], 'launchTime': 6 * 3600000 + 1},
                 {'keyName': 'keyName2', 'instanceId': 'b', 'privateIpAddress': '10.1.1.2', 'publicIpAddress': '2.1.1.1',
-                 "tags": [{"key": "service_name", "value": "foo"}], 'launchTime': 13},
+                 "tags": [{"key": "service_name", "value": "foo"}], 'launchTime': 7 * 3600000 + 1},
                 {'keyName': 'keyName3', 'instanceId': 'c', 'privateIpAddress': '10.1.1.3', 'publicIpAddress': '3.1.1.1',
-                 'securityGroups': ['foobar'], 'launchTime': 14},
+                 'launchTime': 8 * 3600000 + 1},
                 {'keyName': 'keyName4', 'instanceId': 'd', 'privateIpAddress': '10.1.1.4', 'publicIpAddress': '4.1.1.1',
-                 'launchTime': 14},
-                {'instanceId': 'e', 'privateIpAddress': 'x', 'publicIpAddress': 'x', 'launchTime': 5},
-                {'instanceId': 'f', 'privateIpAddress': 'x', 'publicIpAddress': 'x', 'launchTime': 13},
+                 'launchTime': 9 * 3600000 + 1},
+                {'instanceId': 'e', 'privateIpAddress': 'x', 'publicIpAddress': 'x', 'launchTime': 10 * 3600000 + 1},
+                {'instanceId': 'f', 'privateIpAddress': 'x', 'publicIpAddress': 'x', 'launchTime': 11 * 3600000 + 1},
             ]
 
         m = Mock()
         m.query = Mock(side_effect=ret_list)
-        eddaclient.clean = Mock(return_value=m)
-        eddaclient._since = 10
+        eddaclient.soft_clean = Mock(return_value=m)
+        eddaclient._since = 10 * 3600000
+        eddaclient._until = 11 * 3600000
 
         def chef_list(*args, **kwargs):
-            return [{'name': 'host0', 'automatic': {'cloud': {'public_ipv4': '3.1.1.1'}}},
-                    {'name': 'host1', 'automatic': {'cloud': {'public_ipv4': '4.1.1.1'}}},
-                    {'name': 'host2', 'automatic': {'cloud': {'public_ipv4': '10.1.1.1'}}},
-                    {'name': 'host3', 'automatic': {'cloud': {'foo': '2.1.1.1'}}},
-                    {'name': 'host4', 'automatic': {'foo': {'public_ipv4': '2.1.1.1'}}},
-                    {'foo': {'cloud': {'public_ipv4': '2.1.1.1'}}}]
+            return [
+                {'name': 'host0', 'automatic': {'cloud': {'public_ipv4': '5.1.1.1'}}},
+                {'name': 'host1', 'automatic': {'cloud': {'public_ipv4': '6.1.1.1'}}},
+                {'name': 'host2', 'automatic': {'cloud': {'public_ipv4': '7.1.1.1'}}},
+            ]
 
         with patch('plugins.chef.Search', side_effect=chef_list) as MockClass:
             self.plugin.init(eddaclient, self.config, {"first_seen": {'f': 8}}, instance_enricher)
 
-            # run the tested method
-            self.assertEqual(list(self.plugin.do_run()), [
-                             {'id': 'keyName1-tag1', 'plugin_name': 'non_chef', 'details': [
-                              {'tags': {'Name': 'tag1'}, 'instanceId': 'a', 'keyName': 'keyName1', 'securityGroups': [],
-                               'publicIpAddress': '1.1.1.1', 'privateIpAddress': '10.1.1.1', 'service_type': 'tag1',
-                               'elbs': [], 'open_ports': [], 'started': 12}]},
-                             {'id': 'keyName2-foo', 'plugin_name': 'non_chef', 'details': [
-                              {'tags': {'service_name': 'foo'}, 'instanceId': 'b', 'keyName': 'keyName2', 'securityGroups': [],
-                               'publicIpAddress': '2.1.1.1', 'privateIpAddress': '10.1.1.2', 'service_type': 'foo',
-                               'elbs': [], 'open_ports': [], 'started': 13}]}
-                             ])
+            alerts = list(self.plugin.do_run())
+            # there is one problematic node (2.1.1.1)
+            self.assertEqual(1, len(alerts))
+            self.assertTrue(any(a["details"][0]["publicIpAddress"] == "2.1.1.1" for a in alerts))
 
 
 def main():
