12043a38e448e6a032d00822f51d2681462b8d4c
Mykola Kokalko <mykola.kokalko@prezi.com>
2015-04-14 17:05:42 +0200

    [Updated] httpretty version to latest one [Changed] Urllib2 to requests in sso_unportected plugin [Updated] Test for sso_unportected plugin, so they are checking logic correctly [Added] Processing url redirects in threads

--- plugins/sso.py
+++ plugins/sso.py
@@ -1,41 +1,24 @@
 from __future__ import absolute_import
 import logging
 import re
+import requests
 import urllib
-import urllib2
 from multiprocessing import Pool
 from plugins.route53 import load_route53_entries, is_external
-
-
-class MyHTTPErrorProcessor(urllib2.HTTPErrorProcessor):
-
-    def http_response(self, request, response):
-        code, msg, hdrs = response.code, response.msg, response.info()
-
-        if code == 302:
-            return response
-
-        if not (200 <= code < 300):
-            response = self.parent.error(
-                'http', request, response, code, msg, hdrs)
-        return response
-
-    https_response = http_response
 
 
 def page_redirects(location):
     try:
         if location.endswith("."):
             location = location[:-1]
-        opener = urllib2.build_opener(MyHTTPErrorProcessor)
-        page = opener.open(location, timeout=3)
-        code = page.getcode()
+        resp = requests.get(location, allow_redirects=False, timeout=3)
+        code = resp.status_code
         if code == 302:
-            return urllib.unquote(page.headers.getheader('location'))
+            return location, urllib.unquote(resp.headers['location'])
         else:
-            return SSOUnprotected.UNPROTECTED
+            return location, SSOUnprotected.UNPROTECTED
     except:
-        return "-"
+        return location, "-"
 
 
 class SSOUnprotected:
@@ -71,9 +54,8 @@ class SSOUnprotected:
         locations_https = ["https://%s" % name for name in not_aws.keys()]
         locations = list(locations_http + locations_https)
         self.logger.info("fetching %d urls on 16 threads" % len(locations))
-        redirects = {}
-        for l in locations:
-            redirects[l] = page_redirects(l)
+        redirect_items = Pool(16).map(page_redirects, locations)
+        redirects = dict(redirect_items)
         old_redirects = self.status.get("redirects", {})
         alerts = {loc: r for loc, r in redirects.iteritems()
                   if loc not in old_redirects
@@ -96,13 +78,13 @@ class SSOUnprotected:
             #     yield {
             #         "plugin_name": self.plugin_name,
             #         "id": location,
-            #         "details": "This domain (%s) is using GODAUTH" % location
+            #         "details": list(["This domain (%s) is using GODAUTH" % location])
             #     }
             # else:
             #     yield {
             #         "plugin_name": self.plugin_name,
             #         "id": location,
-            #         "details": "This domain (%s) is not reachable" % location
+            #         "details": list(["This domain (%s) is not reachable" % location])
             #     }
 
     def load_aws_ips(self):
