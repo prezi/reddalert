c61014246026e84540895dc3432ce84fc781259e
robert.kiss <robert.kiss@prezi.com>
2015-07-24 10:10:03 +0200

    better error handling for non_chef hosts

--- plugins/chef.py
+++ plugins/chef.py
@@ -2,8 +2,11 @@
 
 from __future__ import absolute_import
 import logging
+import time
+
 import re
 from chef import Search, ChefAPI
+from chef.exceptions import ChefServerError
 
 
 class NonChefPlugin:
@@ -19,7 +22,7 @@ class NonChefPlugin:
         self.edda_client = edda_client
         try:
             self.api = ChefAPI(config['chef_server_url'], config['client_key_file'], config['client_name'])
-        except:
+        except IOError:
             self.logger.exception('Failed to open config file: %s', config['client_key_file'])
             self.api = None
         self.excluded_instances = config.get('excluded_instances', [])
@@ -48,13 +51,22 @@ class NonChefPlugin:
         check_since = since - aws_to_chef_delay
         check_until = until - aws_to_chef_delay
 
-        chef_hosts = {row['automatic']['cloud']['public_ipv4']: row['name']
-                      for row in Search('node', 'ec2:*', rows=1000, api=self.api)
-                      if 'cloud' in row.get('automatic', {}) and    # only store EC2 instances
-                         'public_ipv4' in row['automatic']['cloud']   # which have public IP address
-                      }
+        chef_hosts = []
+        for i in xrange(5):
+            try:
+                search_result = Search('node', 'ec2:*', rows=1000, api=self.api)
+                if search_result:
+                    chef_hosts = {row['automatic']['cloud']['public_ipv4']: row['name']
+                                  for row in search_result
+                                  if 'cloud' in row.get('automatic', {}) and  # only store EC2 instances
+                                  'public_ipv4' in row['automatic']['cloud']  # which have public IP address
+                                  }
+                    break
+            except ChefServerError:
+                time.sleep(5)
 
-        # print chef_hosts
+        if not chef_hosts:
+            self.logger.warning('No chef hosts were found.')
         for machine in self.edda_client.soft_clean().query("/api/v2/view/instances;_expand"):
             launch_time = int(machine.get("launchTime", 0))
 
@@ -62,7 +74,8 @@ class NonChefPlugin:
             tags = {tag['key']: tag['value'] for tag in machine.get('tags', []) if 'key' in tag and 'value' in tag}
             if machine['publicIpAddress'] not in chef_hosts and check_since <= launch_time <= check_until and \
                     not self.is_excluded_instance(tags.get('service_name', None) or tags.get('Name', None)) and \
-                    machine['instanceId'] not in self.status['first_seen'] and machine['publicIpAddress'] != 'null' \
+                            machine['instanceId'] not in self.status['first_seen'] and machine[
+                'publicIpAddress'] != 'null' \
                     and machine['publicIpAddress'] is not None:
                 # found a non-chef managed host which has not been seen before and which is not excluded
                 self.status['first_seen'][machine['instanceId']] = launch_time
@@ -74,6 +87,7 @@ class NonChefPlugin:
                 details = self.instance_enricher.report(machine, extra=extra_details)
                 yield {
                     "plugin_name": self.plugin_name,
-                    "id": "%s-%s" % (machine.get('keyName', machine['instanceId']), machine.get("service_type", "unknown_service")),
+                    "id": "%s-%s" % (
+                        machine.get('keyName', machine['instanceId']), machine.get("service_type", "unknown_service")),
                     "details": [details]
                 }
