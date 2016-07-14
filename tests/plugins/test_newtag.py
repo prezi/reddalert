e171445265848283a73e463faf1585e78292d726
robert.kiss <robert.kiss@prezi.com>
2016-07-14 16:17:15 +0200

    ignore testapp tags

--- tests/plugins/test_newtag.py
+++ tests/plugins/test_newtag.py
@@ -7,7 +7,6 @@ from api import InstanceEnricher
 
 
 class PluginNewInstanceTagTestCase(unittest.TestCase):
-
     def setUp(self):
         self.plugin = NewInstanceTagPlugin()
         self.assertEqual(self.plugin.plugin_name, 'newtag')
@@ -21,7 +20,7 @@ class PluginNewInstanceTagTestCase(unittest.TestCase):
             return [
                 {'imageId': 'ami-1', 'instanceId': 'a', 'launchTime': 400, "tags": [{"key": "Name", "value": "tag1"}]},
                 {'imageId': 'ami-2', 'instanceId': 'b', 'launchTime': 600,
-                    "tags": [{"key": "service_name", "value": "foo"}]},
+                 "tags": [{"key": "service_name", "value": "foo"}]},
                 {'imageId': 'ami-3', 'instanceId': 'c', 'launchTime': 800, "tags":
                     [{"key": "Name", "value": "tag1"}, {"key": "service_name", "value": "foo"}]},
             ]
@@ -42,9 +41,33 @@ class PluginNewInstanceTagTestCase(unittest.TestCase):
 
         m.query.assert_has_calls([call('/api/v2/view/instances;_expand')])
 
+    def test_ignore_tags(self, *mocks):
+        instance_enricher = InstanceEnricher(Mock())
+        eddaclient = Mock()
+        eddaclient._since = 500
+
+        def ret_list(args):
+            return [
+                {'imageId': 'ami-1', 'instanceId': 'a', 'launchTime': 400, "tags": [{"key": "Name", "value": "tag1"}]},
+                {'imageId': 'ami-2', 'instanceId': 'b', 'launchTime': 600,
+                 "tags": [{"key": "service_name", "value": "testapp"}]}
+            ]
+
+        m = Mock()
+        m.query = Mock(side_effect=ret_list)
+        eddaclient.clean = Mock(return_value=m)
+        self.plugin.init(eddaclient, Mock(), {}, instance_enricher)
+
+        # run the tested method
+        result = self.plugin.run()
+        self.assertEqual(0, len(result))
+
+        m.query.assert_has_calls([call('/api/v2/view/instances;_expand')])
+
 
 def main():
     unittest.main()
+
 
 if __name__ == '__main__':
     main()
